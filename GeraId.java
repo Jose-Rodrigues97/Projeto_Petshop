14f08329c0db7865d2d8e4318c139b0f905a78b6
José Evandro Rodrigues Filho <62810148+Jose-Rodrigues97@users.noreply.github.com>
2021-06-30 16:13:23 -0300

    Arquivo gerador de IDs

--- GeraId.java
+++ GeraId.java
@@ -2,7 +2,7 @@ import java.util.UUID;
 
 public abstract class GeraId {
 
-    // #region GERA ID
+    // #region GERA ID 
     public static String criaId() {
         String rand = UUID.randomUUID().toString();
         rand = rand.substring(0, 7);
